5809234a7050582c50cdde5a0e7d9a762878d1fd
Gallo <48700539+gallo96@users.noreply.github.com>
2020-01-23 12:52:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// const http = require('http');
+// http.createServer(function (request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Hello World');
+//   console.log('salut');
+// }).listen(8081);
+
+// console.log('Server running at npm start');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
